5f6b993fa0b77c4957135e2707c9c77cff34b613
gilavi <gilavi@users.noreply.github.com>
2026-05-06 09:31:02 +0000

    deploy: ad30528a01ef92914a1e53f4c8631e7fc6988376

---
+++ app/assets/SubscribeFail-BbJ-fcTf.js
@@ -0,0 +1,6 @@
+import{g as t,j as e,A as r,C as a,b as s,c,e as l}from"./index-yKJos1u4.js";/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=t("CircleX",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m15 9-6 6",key:"1uzhvr"}],["path",{d:"m9 9 6 6",key:"z0biqf"}]]);function x(){return e.jsx(r,{children:e.jsxs(a,{children:[e.jsxs(s,{className:"flex flex-col items-center text-center",children:[e.jsx(n,{className:"mb-2 h-12 w-12 text-danger"}),e.jsx(c,{children:"გადახდა ვერ მოხერხდა"})]}),e.jsx(l,{children:e.jsx("p",{className:"text-center text-sm text-neutral-600",children:"გადახდა გაუქმდა ან მოხდა შეცდომა. დახურეთ ეს ფანჯარა Sarke-ში დასაბრუნებლად."})})]})})}export{x as default};
